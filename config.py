9820e9f2944651551564b096f6ff0b66e44c6040
Hadrien NAUROY <62743655+HadrienNauroy@users.noreply.github.com>
2022-01-13 16:54:51 +0200

    add fake config.py

---
+++ config.py
@@ -0,0 +1,12 @@
+"""C'est le fichier que vous devez modifier avant d'executer le bot"""
+
+
+# indentifiants cascad
+username = "Dupont"
+password = "m0t2Passe"
+
+# Là où ranger les films
+destination = "D:\\films"
+
+# Ne pas toucher sauf si votre connection est trop lente !
+WAIT_TIME = 5
